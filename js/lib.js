85a759d57970323112aac87474f1305676afe03c
Anmo <btavares26@gmail.com>
2014-06-21 20:13:23 +0100

    Update lib.js

--- js/lib.js
+++ js/lib.js
@@ -1,29 +1,46 @@
 Ink.setPath( 'Ink' , 'http://rawgit.com/Anmo/Ink/develop/src/js/Ink/' );
 
-Ink.requireModules( [ 'Ink.Util.Router_1' ] ,
-            function(           Router    ) {
+Ink.requireModules( [ 'Ink.Dom.Css_1' , 'Ink.Util.Router_1' ] ,
+            function(          Css    ,           Router    ) {
     'use strict';
+
+    var body = document.body;
+
+    var main;
+    var num;
+
+    var fail = function( ) { this.setPath( ( main || 'slide' ) + '/1' , true ); };
 
     var router = new Router({
         params : {
             num : '\\d+'
         } ,
-        onFail : function( ) { this.setPath( 'slide/1' ); } ,
+        onFail : fail ,
         paths  : [{
             path  : 'slide' ,
-            enter : function( ) { console.log( 'enter slide' ); } ,
-            fail  : function( ) { this.setPath( 'slide/1' ); } ,
+            enter : function( _main ) {
+                Css.addClassName( body , main = _main );
+            } ,
+            exit  : function( _main ) {
+                Css.removeClassName( body , _main );
+            } ,
+            fail  : fail ,
             paths : [{
                 path : ':num' ,
-                enter : function( num ) { console.log( 'number:' , num ); }
+                enter : function( _num ) { console.log( 'number:' , num = _num ); }
             }]
         } , {
             path  : 'demo' ,
-            enter : function( ) { console.log( 'enter demo' ); } ,
-            fail  : function( ) { this.setPath( 'demo/1' ); } ,
+            enter : function( _main ) {
+                Css.addClassName( body , main = _main );
+            } ,
+            exit  : function( _main ) {
+                Css.removeClassName( body , _main );
+            } ,
+            fail  : fail ,
             paths : [{
                 path : ':num' ,
-                enter : function( num ) { console.log( 'number:' , num ); }
+                enter : function( _num ) { console.log( 'number:' , num = _num ); }
             }]
         }]
     });
