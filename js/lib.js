efb504c3699d6a810d01376f5ed0a595ec57e5a1
Anmo <btavares26@gmail.com>
2014-06-21 19:10:05 +0100

    Update lib.js

--- js/lib.js
+++ js/lib.js
@@ -1,4 +1,28 @@
-Ink.requireModules( [ ] ,
-            function( ) {
+Ink.requireModules( [ 'Ink.Util.Router_1' ] ,
+            function(           Router    ) {
     'use strict';
+
+    var router = new Router({
+        params : {
+            num : '\\d+'
+        } ,
+        onFail : function( ) { this.setPath( 'slide/1' ); } ,
+        paths  : [{
+            path  : 'slide' ,
+            enter : function( ) { console.log( 'enter slide' ); } ,
+            fail  : function( ) { this.setPath( 'slide/1' ); } ,
+            paths : [{
+                path : ':num' ,
+                enter : function( num ) { console.log( 'number:' , num ); }
+            }]
+        } , {
+            path  : 'demo' ,
+            enter : function( ) { console.log( 'enter demo' ); } ,
+            fail  : function( ) { this.setPath( 'demo/1' ); } ,
+            paths : [{
+                path : ':num' ,
+                enter : function( num ) { console.log( 'number:' , num ); }
+            }]
+        }]
+    });
 });
